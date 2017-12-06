aa6cfc8db74c2e57b59270f7c3a37f234777f410
Manju <manjub0419@gmail.com>
2017-12-05 19:28:41 -0800

    Added serach via firstname

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -3,27 +3,44 @@ package hello;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloController {
     
+	Map<String, Contact> contactList;
     @RequestMapping("/")
     public String index() {
         return "Greetings from manju!";
     }
     
+    
     @RequestMapping("/contacts")
-    public Map<String, Contact> getContactList() {
-    	Map<String, Contact> contactList = new HashMap<String, Contact>();
+    public Map<String, Contact> getMatchedContactList(@RequestParam("firstName") String firstName) {
+    	contactList = new HashMap<String, Contact>();
     	contactList.put("1", new Contact("William", "Smith", "williams123@gmail.com", "3761520879"));
     	contactList.put("2", new Contact("Daniel", "Moore", "daniel.moore@gmail.com", "2738098078"));
     	contactList.put("3", new Contact("David", "Harris", "davidharris90@gmail.com", "4920173829"));
     	contactList.put("4", new Contact("Linda", "Nelson", "linda2004@gmail.com", "2910287352"));
     	contactList.put("5", new Contact("Sarah", "Parker", "sarah.parker12@gmail.com", "4891092648"));
-    	return contactList;
+    	if(firstName != null) {
+    		Map<String, Contact> matchedContactList = new HashMap<String, Contact>();
+        	for(Map.Entry<String, Contact> mapEntry: contactList.entrySet()) {
+        		String objectValue =mapEntry.getValue().getFirstName();
+        		String key = mapEntry.getKey();
+        		Contact value = mapEntry.getValue();
+        		if(objectValue.equalsIgnoreCase(firstName)) {
+        			matchedContactList.put(key, value);
+        		}
+        	}
+        	return matchedContactList;
+    	} else {
+    		return contactList;
+    	}
+    	
     }
-    
 }
